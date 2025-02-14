77cf6fc6283c914f015b0996fd34eae734e0ebeb
JJeong <138855764+scarletJeong@users.noreply.github.com>
2025-02-14 17:41:01 +0900

    Delete adm directory

--- adm/admin.menu700.php
+++
@@ -1,5 +0,0 @@
-<?php
-$menu['menu700'] = array(
-    array('700000', '공동구매관리', G5_ADMIN_URL . '/seller_qa.php', 'seller_qa'),
-    array('700100', '셀러/체험단 신청관리', G5_ADMIN_URL . '/seller_qa.php', 'seller_qa')
-);
--- adm/reservation.php
+++
@@ -1,4 +0,0 @@
-<?php
-$sub_menu = "800200";
-include_once('./_common.php');
-?>
